fc697f7141501021b72ba2f1ca2c0877024c7449
ivo1996 <ivelin.dimitrov9@gmail.com>
2021-06-12 16:30:38 +0300

    mvn clean install now works without having to disable tests

--- src/test/java/uni/ruse/welearn/welearn/WeLearnApplicationTests.java
+++ src/test/java/uni/ruse/welearn/welearn/WeLearnApplicationTests.java
@@ -1,14 +1,10 @@
 package uni.ruse.welearn.welearn;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class WeLearnApplicationTests {
-
-	@Autowired
-	TestConfig testConfig;
 
 	@Test
 	void contextLoads() {
